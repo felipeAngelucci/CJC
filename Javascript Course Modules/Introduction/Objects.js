364af07a21dd6424079b46ea362e8899642e8812
Felipe Angelucci <139251927+felipeAngelucci@users.noreply.github.com>
2024-08-19 21:54:01 -0400

    Create Objects.js

---
+++ Javascript Course Modules/Introduction/Objects.js
@@ -0,0 +1,14 @@
+/*
+There are built-in objects in JavaScript, and developers can also build their own objects
+
+One example of a built-in object is the Math object
+
+Objects have methods available to be used
+
+The Math.floor() method takes a decimal number, and rounds down to the nearest whole number.
+*/
+console.log(Math.random() * 100);
+console.log(Math.floor(Math.random() * 100));
+console.log(Math.ceil(43.8));
+// number object is used instead of math object
+console.log(Number.isInteger(2017));
