ff845153a51827fc6a784abdd6e5b83988e8887a
Felipe Angelucci <139251927+felipeAngelucci@users.noreply.github.com>
2024-08-19 22:06:18 -0400

    Create script.js

---
+++ Javascript Course Modules/Introduction/script.js
@@ -0,0 +1,4 @@
+// I made this first JavaScript program on my own using a few things I learned so far from the introduction to Codecademy's JavaScript course.
+console.log('This is my first JavaScript program!');
+console.log('Now I can do many things like talk about the ' + Math.floor(Math.random() * 1000) + ' ' + 'different possibilities of sentences that could be formed when using the math.Random object method!');
+console.log('JavaScript is my new favorite programming language.');
