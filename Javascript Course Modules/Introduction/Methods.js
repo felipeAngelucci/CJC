d2dcc67abf17de8fbebf1089d2c837e0afd368b9
Felipe Angelucci <139251927+felipeAngelucci@users.noreply.github.com>
2024-08-19 21:44:51 -0400

    Create Methods.js

---
+++ Javascript Course Modules/Introduction/Methods.js
@@ -0,0 +1,14 @@
+// Methods:
+// Datatypes have access to methods
+// Use .toUpperCase() to log 'Codecademy' in all uppercase letters
+
+// how to call a method: 
+// 'example string'.methodName()
+
+console.log('Codecademy');
+
+// Use a string method to log the following string without whitespace at the beginning and end of it.
+console.log('    Remove whitespace   '.trim());
+
+// You can find a list of built-in string methods in the JavaScript documentation: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String
+console.log('Codecademy'.toUpperCase());
