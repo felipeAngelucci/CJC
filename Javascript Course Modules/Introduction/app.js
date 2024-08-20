5a19498b6323d4e2bb8c25e93cdb628ad295c7b4
Felipe Angelucci <139251927+felipeAngelucci@users.noreply.github.com>
2024-08-19 20:38:12 -0400

    Create app.js

---
+++ Javascript Course Modules/Introduction/app.js
@@ -0,0 +1,10 @@
+/*
+console is an object
+.log() is a console method & prints to console
+*/
+
+// prints my age on 08/19/24 of 18 in the console
+console.log(18);
+
+// prints weeks I've been programming: 208 weeks
+console.log(208);
