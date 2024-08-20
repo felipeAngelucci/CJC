8040a5bf85a2e5f55eb2f3de125f29cb7660f8a5
Felipe Angelucci <139251927+felipeAngelucci@users.noreply.github.com>
2024-08-19 20:58:18 -0400

    Create DatatypesNotes.js

---
+++ Javascript Course Modules/Introduction/DatatypesNotes.js
@@ -0,0 +1,21 @@
+/*
+In JavaScript, there are eight fundamental data types:
+
+number: any number including with decimals. Ex: 2, 135, 82.15
+
+bigint: Any number, greater than 253-1 or less than -(253-1), with n appended to the number. Ex: 1234567890123456n.
+
+String: Any grouping of characters on your keyboard. Ex: letters, numbers, spaces, symbols, etc, surrounded by single quotes: ' ... ' or double quotes " ... ". Single quotes are preferred professionally.
+
+Boolean: This data type only has two possible values-either true or false.
+
+Null: This data type represents the intentional absence of a value, and is represented by the keyword null.
+
+Undefined: This data type is denoted by the keyword undefined (without quotes). It also represents the absence of a value though it has a different use than null. Undefined means that a given value does not exist.
+
+Symbol: A newer feature to JavaScript. Symbols are unique identifiers, useful in more complex coding.
+
+The seven types above are (basic) primitive data types.
+
+Object: Collections of related data.
+*/
