9341ba65130e1af0f938ca4d5feb0cfc9661dada
Felipe Angelucci <139251927+felipeAngelucci@users.noreply.github.com>
2024-08-19 21:38:26 -0400

    Create Properties.js

---
+++ Javascript Course Modules/Introduction/Properties.js
@@ -0,0 +1,10 @@
+/*
+Properties in JavaScript:
+
+All datatypes have access to specific properties that are passed down to each instance.
+
+For example, every string instance has a property called length that stores the number of characters in that string.
+
+. is the dot operator
+*/
+console.log('Teaching the world how to code'.length);
