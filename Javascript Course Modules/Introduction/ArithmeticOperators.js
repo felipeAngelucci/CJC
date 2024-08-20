b5a1320cc1de7157a76f59943015225c6e82044f
Felipe Angelucci <139251927+felipeAngelucci@users.noreply.github.com>
2024-08-19 21:06:28 -0400

    Create ArithmeticOperators.js

---
+++ Javascript Course Modules/Introduction/ArithmeticOperators.js
@@ -0,0 +1,12 @@
+/*
+An operator is a character that performs a task in our code. 
+
+JavaScript Arithmetic Operators:
+add (+)
+subtract (-)
+multiply (*)
+divide (/)
+modulo/remainder (%)
+
+The modulo operator returns the number that remains after the right-hand number divides into the left-hand number as many times as it evenly can.
+*/
