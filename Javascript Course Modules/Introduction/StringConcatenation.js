3fbc26b33492aecb0d369a784a7783e44096c8e7
Felipe Angelucci <139251927+felipeAngelucci@users.noreply.github.com>
2024-08-19 21:35:30 -0400

    Create StringConcatenation.js

---
+++ Javascript Course Modules/Introduction/StringConcatenation.js
@@ -0,0 +1,15 @@
+/*
+String concatenation: When a + operator is used on two strings, it appends the right string to the left string, or simply puts them together.
+
+console.log('front ' + 'space'); 
+// Prints 'front space'
+console.log('back' + ' space'); 
+// Prints 'back space'
+console.log('no' + 'space'); 
+// Prints 'nospace'
+console.log('middle' + ' ' + 'space'); 
+// Prints 'middle space'
+
+*/
+console.log('Hello' + 'World');
+console.log('Hello' + ' ' + 'World');
