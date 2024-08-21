86a7643c7f9efddaa657757d5338d013164b5a61
Felipe Angelucci <139251927+felipeAngelucci@users.noreply.github.com>
2024-08-20 22:41:21 -0400

    Update Variables.js
    finished lesson one

--- Javascript Course Modules/Variables/Variables.js
+++ Javascript Course Modules/Variables/Variables.js
@@ -1,1 +1,14 @@
-// add code from lesson here
+/*
+Var, short for variable, is a JavaScript keyword that creates, or declares, a new variable.
+
+Camelcase is standard capitalization convention in JavaScript.
+
+Rules for naming variables:
+- Variable names cannot start with numbers.
+- Variable names are case sensitive, so myName and myname would be different variables. It is bad practice to create two variables that have the same name using different cases.
+- Variable names cannot be the same as keywords. All keywords are listed in MDN's keyword documentation: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Lexical_grammar#keywords
+*/
+var favoriteFood = 'pizza';
+var numOfSlices = 8;
+console.log(favoriteFood);
+console.log(numOfSlices);
