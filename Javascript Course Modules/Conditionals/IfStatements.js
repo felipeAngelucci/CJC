200ec171b629b6e1161d2f67f20c3b6a8507d8a5
Felipe Angelucci <139251927+felipeAngelucci@users.noreply.github.com>
2024-08-21 14:13:08 -0400

    Update IfStatements.js

--- Javascript Course Modules/Conditionals/IfStatements.js
+++ Javascript Course Modules/Conditionals/IfStatements.js
@@ -1,1 +1,15 @@
-// This is currently in progress.
+/*
+The if statement is composed of:
+
+The if keyword, which is followed by a set of parentheses () which is followed by a code block, or block statement, indicated by a set of curly braces {}.
+
+Inside the parentheses (), a condition is provided that evaluates to true or false.
+
+If the condition evaluates to true, the code inside the curly braces {} runs, or executes. Otherwise, the code doesn't execute.
+*/
+let sale = true;
+sale = false;
+// If sale is set to false, the code below in the if statement does not run, and nothing is logged in the console.
+if (sale) {
+  console.log('Time to buy!');
+}
