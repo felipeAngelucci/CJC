f65d0d8133963bd60edf596e0e1f36ac2dad5830
Felipe Angelucci <139251927+felipeAngelucci@users.noreply.github.com>
2024-08-21 13:43:34 -0400

    Update JavaScript Documentation.js

--- Resource Catalog/JavaScript Documentation.js
+++ Resource Catalog/JavaScript Documentation.js
@@ -1,4 +1,8 @@
 /*
 https://developer.mozilla.org/en-US/
 You can learn anything and everything from searching the JavaScript MDN documentation.
+
+https://www.codecademy.com/resources/docs
+Codecademy documentation for all topics.
+
 */
