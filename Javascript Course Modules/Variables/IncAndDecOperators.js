76ae57558835684cd1266000664395e22208259f
Felipe Angelucci <139251927+felipeAngelucci@users.noreply.github.com>
2024-08-21 07:42:10 -0400

    Create IncAndDecOperators.js

---
+++ Javascript Course Modules/Variables/IncAndDecOperators.js
@@ -0,0 +1,5 @@
+let gainedDollar = 3;
+let lostDollar = 50;
+gainedDollar++;
+lostDollar--;
+// The increment and decrement operators change the value by 1 and also assign the new value to the variable.
