cde053970eb28cdfb6ec57665db1b4a04a0348ca
Felipe Angelucci <139251927+felipeAngelucci@users.noreply.github.com>
2024-08-21 14:11:24 -0400

    Create Survey.js

---
+++ Javascript Course Modules/Mental Health Survey/Survey.js
@@ -0,0 +1,22 @@
+/*
+This is project 1 of 2 projects I'll build on my own for the conditionals section of this free online course.
+The idea behind this project: Employees at companies need help figuring out whether or not they should seek therapy. They don't want to do a long boring survey so the survey will be concise and helpful.
+This mental health survey will ask questions about their sleep habits, work habits, relationships with family and friends, and other factors that may improve mental health or worsen it.
+Conditional statements will be used to log answers as true or false and if statements will be utilized to deliver a result of whether or not the employee should seek therapy to improve their mental health.
+Task 1:
+Task 2:
+Task 3:
+Task 4:
+Task 5:
+Task 6:
+Task 7:
+Task 8:
+Task 9:
+Task 10:
+Task 11:
+Task 12:
+Task 13:
+Task 14:
+Task 15:
+*/
+// This project will be done once I finish the lesson on conditionals to gain the skills first before testing them with a project.
