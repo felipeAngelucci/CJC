6d7e0d7e69f621db75b00b3ab85b74928a55b551
Felipe Angelucci <139251927+felipeAngelucci@users.noreply.github.com>
2024-08-21 07:47:21 -0400

    Create StringConcatenation.js

---
+++ Javascript Course Modules/Variables/StringConcatenation.js
@@ -0,0 +1,3 @@
+let favoriteAnimal = 'lion';
+// This statement concatenates the string 'my favorite animal: ' with the string value that is stored in the variable favoriteAnimal.
+console.log('My favorite animal: ' + favoriteAnimal)
