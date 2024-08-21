79d76a0f8183521229d704c87680c34b257847ea
Felipe Angelucci <139251927+felipeAngelucci@users.noreply.github.com>
2024-08-21 07:58:56 -0400

    Create StringInterpolation.js

---
+++ Javascript Course Modules/Variables/StringInterpolation.js
@@ -0,0 +1,7 @@
+// My name will never change, ergo, I used a const variable to assign the string value because it was most appropriate.
+const myName = 'Felipe Angelucci';
+// My favorite city will also never change because it holds a special place in my heart.
+// myFavoriteCity
+const myFavoriteCity = 'Rome';
+// The statement below utiizes back ticks (``) to use a template literal by interpolating the variables into readable strings. Template literals also are very efficient since they can be read and understood easily.
+console.log(`My name is ${myName}. My favorite city is ${myFavoriteCity}`)
