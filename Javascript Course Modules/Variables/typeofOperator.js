789eff23c4ac3615dfc2c429deef7246bc2e066c
Felipe Angelucci <139251927+felipeAngelucci@users.noreply.github.com>
2024-08-21 08:04:18 -0400

    Create typeofOperator.js

---
+++ Javascript Course Modules/Variables/typeofOperator.js
@@ -0,0 +1,8 @@
+let newVariable = 'Playing around with typeof.';
+/*
+the typeof operator checks the data type of a variable and returns a string describing the data type.
+*/
+console.log(typeof newVariable);
+newVariable = 1;
+console.log(typeof newVariable);
+// typeof is a unary operator and not a method or variable. All the unary operators in JavaScript are written in lowercase.
