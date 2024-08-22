a95ec0fd570b128090f1c9f75202fa29b531e0e0
Felipe Angelucci <139251927+felipeAngelucci@users.noreply.github.com>
2024-08-21 21:59:00 -0400

    Create LogicalOperators.js

---
+++ Javascript Course Modules/Conditionals/LogicalOperators.js
@@ -0,0 +1,14 @@
+let mood = 'sleepy';
+let tirednessLevel = 6;
+/*
+Logical operators in JavaScript:
+the "and" operator (&&)
+the "or" operator (||)
+the "not" operator, otherwise known as the bang operator (!)
+*/
+if (mood === 'sleepy' || tirednessLevel > 8) {
+  console.log('time to sleep');
+}
+else {
+  console.log('not bed time yet');
+}
