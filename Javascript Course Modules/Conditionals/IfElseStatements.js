76fc2fde66f16e7876f9efbb9c4f8b8e59d1172e
Felipe Angelucci <139251927+felipeAngelucci@users.noreply.github.com>
2024-08-21 20:18:36 -0400

    Create IfElseStatements.js

---
+++ Javascript Course Modules/Conditionals/IfElseStatements.js
@@ -0,0 +1,9 @@
+let sale = true;
+sale = false;
+if(sale) {
+  console.log('Time to buy!');
+}
+// else statements follows the code of if statements. The else statement executes when the if statement's condition is false. If else statements automate solutions to yes-or-no questions.
+else {
+  console.log('Time to wait for a sale.');
+}
