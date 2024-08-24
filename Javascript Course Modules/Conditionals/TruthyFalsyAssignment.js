98936bb2d92847020dde468357b731bee3478260
Felipe Angelucci <139251927+felipeAngelucci@users.noreply.github.com>
2024-08-23 20:56:55 -0400

    Create TruthyFalsyAssignment.js

---
+++ Javascript Course Modules/Conditionals/TruthyFalsyAssignment.js
@@ -0,0 +1,4 @@
+let tool = 'marker';
+// Use short circuit evaluation to assign  writingUtensil variable below:
+let writingUtensil = tool || 'pen';
+console.log(`The ${writingUtensil} is mightier than the sword.`);
