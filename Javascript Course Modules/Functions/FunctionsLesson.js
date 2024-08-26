eb2774e952ccc5db189848e9be77a0452b900cae
Felipe Angelucci <139251927+felipeAngelucci@users.noreply.github.com>
2024-08-26 13:51:06 -0400

    Update FunctionsLesson.js

--- Javascript Course Modules/Functions/FunctionsLesson.js
+++ Javascript Course Modules/Functions/FunctionsLesson.js
@@ -1,2 +1,11 @@
 // A function is a reusable block of code that groups together a sequence of statements to perform a specific task.
-// the code is in progress to be finished tomorrow.
+// Example of a function below
+function greetWorld() {
+  console.log('Hello, World!');
+}
+// Hoisting is when a function is called before it is defined. Hoisting isn't usually a good practice but it's a feature in JavaScript that can be used.
+// for example:
+countToThree();
+function countToThree() {
+  console.log('One, two, three.');
+}
