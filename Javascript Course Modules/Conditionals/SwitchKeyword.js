9f9dcb5a1786c6e2c915cf0bcd328868172592eb
Felipe Angelucci <139251927+felipeAngelucci@users.noreply.github.com>
2024-08-25 21:16:52 -0400

    Create SwitchKeyword.js

---
+++ Javascript Course Modules/Conditionals/SwitchKeyword.js
@@ -0,0 +1,16 @@
+let athleteFinalPosition = 'first place';
+// Switch statements make it easier and cleaner to check conditions with many conditional statements.
+switch (athleteFinalPosition) {
+  case 'first place':
+    console.log('You get the gold medal!');
+    break;
+  case 'second place':
+    console.log('You get the silver medal!');
+    break;
+  case 'third place':
+    console.log('You get the bronze medal!');
+    break;
+  default:
+    console.log('No medal awarded.');
+    break;
+}
