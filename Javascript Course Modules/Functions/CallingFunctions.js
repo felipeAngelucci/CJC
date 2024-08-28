702f4fdf92e8b4d5d42cf4b4f53e07eb3706a948
Felipe Angelucci <139251927+felipeAngelucci@users.noreply.github.com>
2024-08-27 22:20:56 -0400

    Create CallingFunctions.js

---
+++ Javascript Course Modules/Functions/CallingFunctions.js
@@ -0,0 +1,7 @@
+// Functions only execute when called with the function name followed by parentheses.
+function sayThanks() {
+  console.log('Thank you for your purchase! We appreciate your business.');
+}
+sayThanks();
+sayThanks();
+sayThanks();
